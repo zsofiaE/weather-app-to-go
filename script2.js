c1c1b5e443c403c2559c4e481f8505b00a9ed665
Zsofia <you@example.com>
2023-02-01 14:51:25 +0100

    fetched data from unspplash api as bg

--- script2.js
+++ script2.js
@@ -1,45 +1,64 @@
-const clientId = "befOWXko_VO5MAUZByFqicKzazdX8IHU6gsSb6BXadc"; // Nr. 1
-//const clientId = "8FAQOMjMmHK-HJmM7eLkhcRwyWZVg_TZ5ybi20qw9Dw"; // Nr. 2
+//const clientId = "befOWXko_VO5MAUZByFqicKzazdX8IHU6gsSb6BXadc"; // Nr. 1
+const clientId = "8FAQOMjMmHK-HJmM7eLkhcRwyWZVg_TZ5ybi20qw9Dw"; // Nr. 2
 //const clientId = "f-pm4kGZgNh2VI_tYGJHAV2pEUazn_deA1KiMjMQUYg"; // Nr. 3
 // const clientId = "h_3sisufaSWfhCD0Zsog_yjhnKj-yr1RTyjhuKr3VFY"; // Nr. 4 
 // const clientId = "nhHInUCkFEGWWmSpsJt3pJLTYevYEE1oy4pvyWatKiM"; // Nr. 5
 
+let result;
 
 
-// fetch('http://example.com/songs')
-// 	.then(response => response.json())
-// 	.then(data => console.log(data))
-// 	.catch(err => console.error(err));
-
-
-function searchWeather(){
+function searchPhotos(){
     let searchInput = document.getElementById("search-input").value;
-    const url = `https://api.unsplash.com/search/photos/?client_id=${clientId}&query=${searchInput}&per_page=1`;
+    const url = `https://api.unsplash.com/search/photos/?client_id=${clientId}&query=${searchInput}&per_page=5`;
+    let searchSection = document.querySelector('search-section');
+    const gallery = document.querySelector(".search-section");
 
-
-    // fetch(url)
-    // .then (function (data) {
-    //     return data.json();
-    // })  .then (function (data) {
+    fetch(url)
+    .then (function (data) {
+        //console.log(data)
+    return data.json();
+    })  .then (function (data) {
+        console.log(data) //array/results
+       
     //     return data
     // })
     //  .then(function (jsonData) {
-    //    //console.log(jsonData.results)
-    //  } )
-      
-    // }
-     
-    fetch('url')
-	.then(response => response.json())
-	.then(data => console.log(data))
-	.catch(err => console.error(err));
+    //    console.log(jsonData)
+
+	//.catch(err => console.error(err));
+
+    //result = jsonData.results
+
+    console.log(data.results) //array/0
+
+    const bg = document.querySelector(".bg");
+    const img = data.results[Math.floor(Math.random() * 5)].urls.regular;
+    bg.style.backgroundImage = `url(${img})`;
+
+   //data.results.forEach ( photo => {
+
+    //     let bgImage = "";
+    //     bgImage = document.createElement('img');
+    //     bgImage.src = photo.urls.regular;
+    //     bgImage.className = "search-section";
+    //     //`<img src="${photo.urls.regular}">`
+    //    searchSection.appendChild(bgImage);
+
+    // let bgImg = "";
+    // bgImg = document.createElement('img');
+    // bgImg.src = photo.urls.regular;
+    // bgImg.className = "search-section";
+    // gallery.appendChild(bgImg);
+
+    // document.body.style.backgroundImage = 'url(${data.results[0].urls.regular})';
+
+   // document.body.style.backgroundImage = 'url(${data.results.urls.regular})';
+           //console.log(bgImage)
+
+//    })
+})
 
 
+}
 
-    const searchSection = document.querySelector(".search-section");
-    searchSection.innerHTML="";
-    let bgImage = document.createElement('img');
-    bgImage.src = 
-
-
-
+//searchPhotos()
