394839f9fe7cb5c7073ebe01d6a2d32b270922aa
Zsofia <you@example.com>
2023-02-02 09:41:17 +0100

    time tone change

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const clientId = "8FAQOMjMmHK-HJmM7eLkhcRwyWZVg_TZ5ybi20qw9Dw"; // Nr. 2
 // const clientId = "nhHInUCkFEGWWmSpsJt3pJLTYevYEE1oy4pvyWatKiM"; // Nr. 5
 
 let result;
-const city = document.querySelector(".city");
+
 
 
 
@@ -40,7 +40,7 @@ function searchPhotos(){
     const weatherCard = document.querySelector(".weather-card");
 
     weatherCard.classList.remove('hide');
-    city.innerHTML = searchInput;
+   // city.innerHTML = searchInput; //we add City rathe rfrom API
     
     
     
@@ -75,25 +75,31 @@ const weatherUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${
 const icon = document.querySelector(".icon");
 const weather = document.querySelector(".weather");
 const temp = document.querySelector(".tempreture");
+const localTime2 = document.querySelector(".local-time2");
+const city = document.querySelector(".city");
 
 
  fetch(weatherUrl)
 	.then(response => response.json())
 	.then(data => {
         console.log(data)
+        city.innerHTML = data.location.name;
         icon.src = data.current.condition.icon;
         weather.innerText = data.current.condition.text;
         temp.innerText = data.current.temp_c + "°C";
+        localTime2.innerText = data.location.localtime;
 
-        var today = new Date();
-        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-        var time1 = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        var dateTime = date+' '+time1;
+// function for adding a time+Date
+//         var today = new Date();
+//         var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+//         var time1 = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//         var dateTime = date+' '+time1;
  
-console.log(dateTime)
-        let localTime = document.querySelector(".local-time");
-        localTime.innerText = dateTime;
+// console.log(dateTime)
+//         let localTime = document.querySelector(".local-time");
+//         localTime.innerText = dateTime;
 
+// function for adding live time - but it is still my local, not the local, based on input city
         let a;
         let time;
         setInterval(() => {
@@ -101,6 +107,17 @@ console.log(dateTime)
           time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
           document.getElementById('time').innerHTML = "Local time: " + time;
         }, 1000);
+
+
+
+       const timeZoneUrl = `http://api.weatherapi.com/v1/timezone.json?key=${apiKey}&q=${searchInput}`;
+       fetch(timeZoneUrl)
+       .then(response => response.json())
+       .then(datatz => {
+           console.log(datatz)
+
+       })
+
 
      } )
 	//.catch(err => console.error(err));
