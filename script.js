1af4026530b69f53e081cfd76cd926628719e67d
mimi-sln~ <mimuska.chlebanova@gmail.com>
2023-02-05 19:22:51 +0100

    added Labels of the current day and made the cards a little bit smaller.

--- script.js
+++ script.js
@@ -38,19 +38,21 @@ function createCard(Wettervorhersage)
 {
   let content = `
   <div class="weather-card${Wettervorhersage.cardNumber}">
+  <div Class="labelNumber${Wettervorhersage.cardNumber}">
+  </div>
     <h1 class="city${Wettervorhersage.cardNumber}"></h1>
     <h2 class="country${Wettervorhersage.cardNumber}"></h2>
     <div class="forecast${Wettervorhersage.cardNumber}">
-        <image class="icon${Wettervorhersage.cardNumber}" src="/assets/apple-touch-icon.png">
+        <image class="icon${Wettervorhersage.cardNumber}" src="">
         <p class="weather${Wettervorhersage.cardNumber}"></p>
         <p class="tempreture${Wettervorhersage.cardNumber}"></p>  
     </div>
     <div class="container">
-        <div class="extra-data">
+        <div class="astro">
             <p class="humidity${Wettervorhersage.cardNumber}"></p>
             <p class="uv${Wettervorhersage.cardNumber}"></p>
         </div>
-        <div class="extra-data astro">
+        <div class="extra-data">
             <div class="box">
               <img src=${Wettervorhersage.sunrise} width="24" height="24">
               <p class="sunrise${Wettervorhersage.cardNumber}"></p>
@@ -87,9 +89,9 @@ offersGrid.appendChild(card);
 
 //const clientId = "befOWXko_VO5MAUZByFqicKzazdX8IHU6gsSb6BXadc"; // Nr. 1
 //const clientId = "8FAQOMjMmHK-HJmM7eLkhcRwyWZVg_TZ5ybi20qw9Dw"; // Nr. 2
-const clientId = "f-pm4kGZgNh2VI_tYGJHAV2pEUazn_deA1KiMjMQUYg"; // Nr. 3
+//const clientId = "f-pm4kGZgNh2VI_tYGJHAV2pEUazn_deA1KiMjMQUYg"; // Nr. 3
 // const clientId = "h_3sisufaSWfhCD0Zsog_yjhnKj-yr1RTyjhuKr3VFY"; // Nr. 4 
-// const clientId = "nhHInUCkFEGWWmSpsJt3pJLTYevYEE1oy4pvyWatKiM"; // Nr. 5
+ const clientId = "nhHInUCkFEGWWmSpsJt3pJLTYevYEE1oy4pvyWatKiM"; // Nr. 5
 
 let result;
 function searchPhotos(theCityName)
@@ -123,7 +125,7 @@ function apiCall (cityName, dayNumber)
   const timeDate = document.querySelector(`.time-date${dayNumber}`);
   const city = document.querySelector(`.city${dayNumber}`);
   const country = document.querySelector(`.country${dayNumber}`);
-  
+
  // fechting data for the chosen Destination
  fetch(weatherUrl)
 	.then(response => response.json())
@@ -135,12 +137,11 @@ function apiCall (cityName, dayNumber)
       icon.src = data.current.condition.icon;
       weather.innerText = data.current.condition.text;
       temp.innerText = data.current.temp_c + "°C";
-      timeDate.innerText = `Local DT in ${cityName}: ${data.location.localtime}`;
+      timeDate.innerText = `DT in ${cityName}: ${data.location.localtime}`;
       
-
       // humidity 
       const humidity = document.querySelector(`.humidity${dayNumber}`);
-      humidity.innerText = "Humidity: " + data.current.humidity +"% ";
+      humidity.innerText = "Humidity: " + data.current.humidity +"%";
       
       // UV 
       const uv = document.querySelector(`.uv${dayNumber}`);
@@ -170,12 +171,20 @@ function apiCall (cityName, dayNumber)
         }
       )
       
+ 
         fetch(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=3&dt=${today}`)
         .then(response => response.json())
         .then(data => 
         {
           console.log(data.forecast.forecastday[1]);
           console.log(data.forecast.forecastday[2]);
+       
+          //--> show the Label of the day 1
+          const theDay1 = document.querySelector(`.labelNumber1`);
+          const label1 = document.createElement("label");
+          label1.textContent = "Today";
+          label1.classList.add('labelNumber');
+          theDay1.appendChild(label1);
 
           // show forecast for the second day
           const timeDate2 = document.querySelector(`.time-date2`);
@@ -185,16 +194,22 @@ function apiCall (cityName, dayNumber)
           const sunrise2 = document.querySelector(`.sunrise2`);
           const sunset2 = document.querySelector(`.sunset2`);
           const humidity2 = document.querySelector(`.humidity2`);
-          const uv2 = document.querySelector(`.uv2`);
-
+          const uv2 = document.querySelector(`.uv2`);        
+         
           timeDate2.innerText = data.forecast.forecastday[1].date;
           temp2.innerText = data.forecast.forecastday[1].day.avgtemp_c;
           icon2.innerText = data.forecast.forecastday[1].day.condition.icon;
           weather2.innerText = data.forecast.forecastday[1].day.condition.text;
           sunrise2.innerText = data.forecast.forecastday[1].astro.sunrise;
           sunset2.innerText = data.forecast.forecastday[1].astro.sunset;
-          humidity2.innerText = "Humidity: " + data.forecast.forecastday[1].day.avghumidity + "% ";
+          humidity2.innerText = "Humidity: " + data.forecast.forecastday[1].day.avghumidity + "%";
           uv2.innerText = "UV Index: " + data.forecast.forecastday[1].day.uv;
+          //--> show the Label of the day 2
+          const theDay2 = document.querySelector(`.labelNumber2`);
+          const label2 = document.createElement("label");
+          label2.textContent = "Tomorrow";
+          label2.classList.add('labelNumber');
+          theDay2.appendChild(label2);
 
           // show forecast for the third day
           const timeDate3 = document.querySelector(`.time-date3`);
@@ -214,6 +229,12 @@ function apiCall (cityName, dayNumber)
           sunset3.innerText = data.forecast.forecastday[2].astro.sunset;
           humidity3.innerText = "Humidity: " + data.forecast.forecastday[2].day.avghumidity + "% ";
           uv3.innerText = "UV Index: " + data.forecast.forecastday[2].day.uv;   
+          //--> show the Label of the day 3
+          const theDay3 = document.querySelector(`.labelNumber3`);
+          const label3 = document.createElement("label");
+          label3.textContent = "Day after tomorrow";
+          label3.classList.add('labelNumber');
+          theDay3.appendChild(label3);
         }
       )
     }
@@ -225,10 +246,11 @@ function apiCall (cityName, dayNumber)
 document.querySelector('#search-input').addEventListener('change', function() {
   var city = document.getElementById('search-input').value;
   apiCall(city, 1);
-  searchPhotos(city);
-  
   const weatherCard = document.querySelector(".gridContainer");
   weatherCard.classList.remove('hide'); 
+
+  searchPhotos(city);
+  
 });
 
 
